7a2eb3f2a13b50838e7d102d930ce4b77411b58c
LuisTaborda <luisgustavo.taborda@gmail.com>
2020-04-01 16:19:17 -0300

    praticando type script

---
+++ app.ts
@@ -0,0 +1,13 @@
+import {Spacecraft, Containership} from './base-ships';
+import {MillenniumFalcon} from './starfighters';
+
+let ship = new Spacecraft('hyperdrive');
+ship.jumpIntoHyperspace();
+
+
+let falcon = new MillenniumFalcon();
+falcon.jumpIntoHyperspace();
+
+
+let  goodForTheJob = (ship: Containership) => ship.cargoContainers > 2;
+console.log(`Is falcon good for the job? ${goodForTheJob( falcon ) ? 'YES' : 'NO'}`);
